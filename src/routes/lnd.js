7e413be6d4fda85cd3257901ef9025913cb039af
Otavio Bonder <otaviobonder@gmail.com>
2019-10-27 16:46:13 -0300

    created controller for some lnd functions created new routes

--- src/routes/lnd.js
+++ src/routes/lnd.js
@@ -2,10 +2,11 @@ const express = require("express");
 const routes = express.Router();
 const lnd = require("../middlewares/lnService");
 const lnService = require("ln-service");
+const lightningController = require("../controllers/lightning");
 
 routes.get("/listchannels", async (req, res) => {
   const channels = await lnService.getChannels({ lnd });
-  return res.json(channels);
+  return res.json(lightningController.getChannels(channels.channels));
 });
 
 routes.get("/forwardingreputation", async (req, res) => {
@@ -31,6 +32,11 @@ routes.get("/uri", async (req, res) => {
 routes.get("/watchtower", async (req, res) => {
   const watchtower = await lnService.getTowerServerInfo({ lnd });
   return res.json(watchtower.tower.uris[0]);
+});
+
+routes.get("/getalias/:id", async (req, res) => {
+  const info = await lnService.getNode({ lnd, public_key: req.params.id });
+  return res.json(info.alias);
 });
 
 module.exports = routes;
